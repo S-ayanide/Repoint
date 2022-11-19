d241e501877c74b6caf47ccbd96db2ae3eb37fc7
Sayan Mondal <sayan.mondal@harness.io>
2022-11-19 19:45:02 +0530

    chore: Adding Quicksand font

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <span>Hero Banner</span>
+
+      <div className="products-heading">
+        <h2>Black Friday Top Picks</h2>
+        <p>From the biggest designer brands, to the best of luxury and hidden gems.</p>
+      </div>
+
+      <div className="products-container">
+        {['Product 1', 'Product 2', 'Product 3', 'Product 4'].map((product) => product)}
+      </div>
+
+      <span>Footer</span>
+    </div>
+  );
 };
 
 export default Home;
