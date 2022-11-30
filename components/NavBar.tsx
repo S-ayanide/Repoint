191529c3d0e6b5cde736680e0cadd853235ac028
Sayan Mondal <sayan.mondal@harness.io>
2022-11-30 20:01:06 +0530

    chore: Adding Navbar and Footer

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,7 +1,20 @@
+import Link from 'next/link';
 import React from 'react';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">Repoint</Link>
+      </p>
+
+      <button type="button" className="cart-icon" onClick={() => void 0}>
+        <AiOutlineShoppingCart />
+        <span className="cart-item-qty">1</span>
+      </button>
+    </div>
+  );
 };
 
 export default NavBar;
