12df70c618abdb0b2765387d98b6047e8660a6e7
Sayan Mondal <sayan.mondal@harness.io>
2022-11-30 19:54:37 +0530

    chore: Adding Layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,27 @@
+import Head from 'next/head';
 import React from 'react';
+import Footer from './Footer';
+import NavBar from './NavBar';
 
-const Layout = () => {
-  return <div>Layout</div>;
+interface ILayout {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: ILayout) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>Repoint</title>
+      </Head>
+      <header>
+        <NavBar />
+      </header>
+      <main className="main-container">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default Layout;
