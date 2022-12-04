ef4a5fd5ad5fa8b05f265b2e789bcbf941b7b65e
Sayan Mondal <sayan.mondal@harness.io>
2022-12-04 15:45:25 +0530

    fix: Price to 2 decimal digits

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -42,7 +42,7 @@ const Cart = () => {
                 <div className="item-desc">
                   <div className="flex top">
                     <h3>{item.name}</h3>
-                    <h3>₹{item.price}</h3>
+                    <h3>₹{item.price.toFixed(2)}</h3>
                   </div>
                   <div className="flex bottom">
                     <div>
@@ -70,7 +70,7 @@ const Cart = () => {
           <div className="cart-bottom">
             <div className="total">
               <h3>Subtotal:</h3>
-              <h3>₹{totalPrice}</h3>
+              <h3>₹{totalPrice.toFixed(2)}</h3>
             </div>
             <div className="btn-container">
               <button type="button" className="btn" onClick={void 0}>
